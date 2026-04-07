80afb29c44eed350d2e85eaaa372327f2da5db3b
Edwin Rodríguez <edro7201@gmail.com>
2026-04-06 21:12:23 -0600

    Para Deploy

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 export const INSTITUCION = "Instituto Teológico de las Asambleas de Iglesias Cristianas"; // Nombre de la aplicación  
 export const SHORT_APP_NAME = "ITAIC"; // Nombre corto de la aplicación 
 
+export const API_BASE_URL = "https://opnlms.onrender.com/api/v1"; // URL base de la API
